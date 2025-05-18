b58d4e01b1a1251e5c6cbd79d5d8ba3dadd8dacd
Ayush Goel <117983314+Ayush-Goel20@users.noreply.github.com>
2025-05-18 19:09:44 +0530

    Update test3.c

--- Lexical Analyzer/test3.c
+++ Lexical Analyzer/test3.c
@@ -1,8 +1,34 @@
-
 #include<stdio.h>
-void main()
-{
-	int a=5;
-	//identifier rule broken
-	float b = 9.5;					
+double f(double x){
+    return 1/(1+x*x);
 }
+int main()
+{
+    double a,b,h,result;
+    int n;
+    printf("Enter lower limit a");
+    scanf("%lf",&a);
+    printf("Enter upper limit a");
+    scanf("%lf",&b);  
+    printf("Enter no of intervals");
+    scanf("%d",&n);
+    if(n%2!=0){
+        printf("error");
+        return 1;
+    } 
+    h=(b-a)/n;
+    result=f(a)+f(b);
+    for(int i=1;i<n;i++){
+        double x=a+i*h;
+        if(i%2==0){
+            result+=2*f(x);
+        }
+        else{
+            result+=4*f(x);
+        }
+    }
+    result=result*(h/3);
+    printf("%lf",result);
+    return 0;
+}
+
